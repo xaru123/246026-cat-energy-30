c8048779fa185b2f9ab1a42609f1f87ebc32aad8
Alena Bagaeva <xaru123@yandex.ru>
2024-01-12 02:13:13 +0500

    refactor + catalog.mobile

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,68 +4,72 @@ createMap();
 setSlider();
 
 function toggleMenu() {
-  const navMain = document.querySelector('.main-nav');
-  const toggle = document.querySelector('.main-header__toggle');
+    const navMain = document.querySelector('.main-nav');
+    const toggle = document.querySelector('.main-header__toggle');
 
-  if (!navMain) {
-    return ;
-  }
-
-  navMain.classList.remove('main-nav--nojs');
-
-  toggle.addEventListener('click', () => {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-      toggle.classList.add('main-header__toggle--closed');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-      toggle.classList.remove('main-header__toggle--closed');
+    if (!navMain) {
+        return;
     }
-  });
+
+    navMain.classList.remove('main-nav--nojs');
+
+    toggle.addEventListener('click', () => {
+        if (navMain.classList.contains('main-nav--closed')) {
+            navMain.classList.remove('main-nav--closed');
+            navMain.classList.add('main-nav--opened');
+            toggle.classList.add('main-header__toggle--closed');
+        } else if (navMain.classList.contains('main-nav--opened')) {
+            navMain.classList.add('main-nav--closed');
+            navMain.classList.remove('main-nav--opened');
+            toggle.classList.remove('main-header__toggle--closed');
+        } else if (
+            !navMain.classList.contains('main-nav--closed') && !navMain.classList.contains('main-nav--opened')) {
+            navMain.classList.add('main-nav--opened');
+            toggle.classList.add('main-header__toggle--closed');
+        }
+    });
 }
 
 function createMap() {
-  // eslint-disable-next-line no-undef
-  const yandexMapApi = ymaps;
+    // eslint-disable-next-line no-undef
+    const yandexMapApi = ymaps;
 
-  yandexMapApi.ready(init);
+    yandexMapApi.ready(init);
 
-  function init() {
-    if (!document.querySelectorAll('#map').length) {
-      return;
+    function init() {
+        if (!document.querySelectorAll('#map').length) {
+            return;
+        }
+
+        const myMap = new yandexMapApi.Map('map', {
+                center: [59.938631, 30.323037],
+                zoom: 14,
+                controls: []
+            }),
+
+            myPieChart = new yandexMapApi.Placemark(
+                [59.938631, 30.323037],
+                {
+                    hintContent: 'Собственный значок метки',
+                    balloonContent: 'Это красивая метка'
+                }, {
+                    iconLayout: 'default#image',
+                    iconImageHref: 'images/map-pin@1x.png',
+                    iconImageSize: [57, 53],
+                    iconImageOffset: [-5, -38]
+                });
+
+        myMap.geoObjects
+            .add(myPieChart);
     }
-
-    const myMap = new yandexMapApi.Map('map', {
-        center: [59.938631, 30.323037],
-        zoom: 14,
-        controls: []
-      }),
-
-      myPieChart = new yandexMapApi.Placemark(
-        [59.938631, 30.323037],
-        {
-          hintContent: 'Собственный значок метки',
-          balloonContent: 'Это красивая метка'
-        }, {
-          iconLayout: 'default#image',
-          iconImageHref: 'images/map-pin@1x.png',
-          iconImageSize: [57, 53],
-          iconImageOffset: [-5, -38]
-        });
-
-    myMap.geoObjects
-      .add(myPieChart);
-  }
 }
 
-function setSlider () {
-  const range = document.querySelector('.example-image__range');
+function setSlider() {
+    const range = document.querySelector('.example-image__range');
 
-  if (range) {
-    range.addEventListener('input', (e) => {
-      e.target.parentNode.style.setProperty('--value', `${e.target.value}%`);
-    });
-  }
+    if (range) {
+        range.addEventListener('input', (e) => {
+            e.target.parentNode.style.setProperty('--value', `${e.target.value}%`);
+        });
+    }
 }
